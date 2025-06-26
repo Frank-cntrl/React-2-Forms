ad5b073e8e9a719d47c242b7b2ab9fc05ac2581b
Joseph <sourpina@gmail.com>
2025-06-26 01:53:43 -0400

    Added author to book card display
    Also fixed ratings on initial books not being from 1-5

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -8,6 +8,7 @@ const BookCard = ({ book }) => {
         alt={book.title}
       />
       <h4 className="book-title">{book.title || "Untitled Book"}</h4>
+      <p className="book-author">{book.author}</p>
       <p className="book-rating">Rating: {book.rating}</p>
       <a
         href={book.url}
